a38e4b3d4af85e760f1573cb0d84a4aafee384aa
Jean-PierrePapin <skouo82@gmail.com>
2021-07-06 17:40:19 +0100

    Rebuilding the app with Vue

--- gs-01-starting-project/app.js
+++ gs-01-starting-project/app.js
@@ -1,4 +1,22 @@
-const buttonEl = document.querySelector('button');
+Vue.createApp({
+    data() {
+        return {
+            goals: [],
+            enteredValue: ''
+        };
+    },
+    methods: {
+        addGoal() {
+            this.goals.push(this.enteredValue);
+            this.enteredValue = '';
+        }
+    }
+}).mount('#app');
+
+
+
+
+/* const buttonEl = document.querySelector('button');
 const inputEl = document.querySelector('input');
 const listEl = document.querySelector('ul');
 
@@ -10,4 +28,4 @@ function addGoal() {
     inputEl.value = '';
 }
 
-buttonEl.addEventListener('click', addGoal);
+buttonEl.addEventListener('click', addGoal); */
