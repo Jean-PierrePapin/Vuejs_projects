ed33e1720db5736c68fc4b20eb8a60ec69939524
Jean-PierrePapin <skouo82@gmail.com>
2021-08-02 10:16:14 +0100

    Creating a basic Vue app

---
+++ vue-cli-01-a-new-vue-project/src/App.vue
@@ -0,0 +1,31 @@
+<template>
+    <section>
+        <h2>My Friends</h2>
+        <ul>
+            <li></li>
+        </ul>
+    </section>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            friends: [
+                {
+                    id: 'manuel',
+                    name: 'Manuel Lorenz',
+                    phone: '0123 45678 9',
+                    email: 'manuel@local.com'
+                },
+                {
+                    id: 'Sam',
+                    name: 'Sam V',
+                    phone: '0123 78945 9',
+                    email: 'sam@local.com'
+                }
+            ]
+        };
+    }
+};
+</script>
