d4db32b9e8a46125b1a4a8cc167326ec38dfc7a4
Jean-PierrePapin <skouo82@gmail.com>
2021-08-06 09:09:27 +0100

    Setup the project

---
+++ prj-cmp-01-starting-setup/src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <ul></ul>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      storedResources: [
+        {
+          id: 'official-guide',
+          title: 'Official Guide',
+          description: 'The official Vues.js documentation',
+          link: 'https://vuejs.org'
+        },
+        {
+          id: 'google',
+          title: 'Google',
+          description: 'Learn to google...',
+          link: 'https://google.com'
+        }
+      ]
+      ]
+    };
+  }
+};
+</script>
