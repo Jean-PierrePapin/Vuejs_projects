974a66ec7670b5af523e950b3727ec60a908b43f
Jean-PierrePapin <skouo82@gmail.com>
2021-09-05 14:17:09 +0100

    Adding a signup action & fmpw

--- main-prj-01-starting-setup/src/store/modules/auth/actions.js
+++ main-prj-01-starting-setup/src/store/modules/auth/actions.js
@@ -1,1 +1,29 @@
-export default {};
+export default {
+    login() {},
+    async signup(context, payload) {
+        // Copy and paste the API key from the firebase project parameters
+        const response = await fetch('https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyBnTgN_d3IeaG6BfFJ6n6iaeOG_f3scyE4', {
+            method: 'POST',
+            body: JSON.stringify({
+                email: payload.email,
+                password: payload.password,
+                returnSecureToken: true
+            })
+        });
+
+        const responseData = await response.json();
+
+        if (!response.ok) {
+            console.log(responseData);
+            const error = new Error(responseData.message || 'Failed to authenticate.');
+            throw error;
+        }
+
+        console.log(responseData);
+        context.commit('setUser', {
+            token: responseData.idToken,
+            userId: responseData.localId,
+            tokenExpiration: responseData.expiresIn
+        });
+    }
+};
