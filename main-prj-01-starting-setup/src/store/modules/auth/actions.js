d388aa06e49b2e97fb7332d5cf6fe55674976eed
Jean-PierrePapin <skouo82@gmail.com>
2021-09-11 13:59:23 +0100

    Adding auto login

--- main-prj-01-starting-setup/src/store/modules/auth/actions.js
+++ main-prj-01-starting-setup/src/store/modules/auth/actions.js
@@ -1,6 +1,24 @@
 export default {
     async login(context, payload) {
-        const response = await fetch('https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=AIzaSyBnTgN_d3IeaG6BfFJ6n6iaeOG_f3scyE4', {
+        return context.dispatch('auth', {
+            ...payload,
+            mode: 'login'
+        });
+    },
+    async signup(context, payload) {
+        return context.dispatch('auth', {
+            ...payload,
+            mode: 'signup'
+        });
+    },
+    async auth(context, payload) {
+        const mode = payload.mode;
+        let url = 'https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=AIzaSyBnTgN_d3IeaG6BfFJ6n6iaeOG_f3scyE4';
+
+        if (mode === 'signup') {
+            url = 'https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyBnTgN_d3IeaG6BfFJ6n6iaeOG_f3scyE4';
+        }
+        const response = await fetch(url, {
             method: 'POST',
             body: JSON.stringify({
                 email: payload.email,
@@ -12,43 +30,30 @@ export default {
         const responseData = await response.json();
 
         if (!response.ok) {
-            console.log(responseData);
             const error = new Error(responseData.message || 'Failed to authenticate.');
             throw error;
         }
 
-        console.log(responseData);
+        localStorage.setItem('token', responseData.idToken);
+        localStorage.setItem('userId', responseData.localId);
+
         context.commit('setUser', {
             token: responseData.idToken,
             userId: responseData.localId,
             tokenExpiration: responseData.expiresIn
         });
     },
-    async signup(context, payload) {
-        // Copy and paste the API key from the firebase project parameters
-        const response = await fetch('https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyBnTgN_d3IeaG6BfFJ6n6iaeOG_f3scyE4', {
-            method: 'POST',
-            body: JSON.stringify({
-                email: payload.email,
-                password: payload.password,
-                returnSecureToken: true
-            })
-        });
+    tryLogin(context) {
+        const token = localStorage.getItem('token');
+        const userId = localStorage.getItem('userId');
 
-        const responseData = await response.json();
-
-        if (!response.ok) {
-            console.log(responseData);
-            const error = new Error(responseData.message || 'Failed to authenticate.');
-            throw error;
+        if (token && userId) {
+            context.commit('setUser', {
+                token: token,
+                userId: userId,
+                tokenExpiration: null
+            });
         }
-
-        console.log(responseData);
-        context.commit('setUser', {
-            token: responseData.idToken,
-            userId: responseData.localId,
-            tokenExpiration: responseData.expiresIn
-        });
     },
     logout(context) {
         context.commit('setUser', {
